3e8626f25af8a93b31add0a7632e9c43a486c796
Joshua Kardell <kardelljoshua@gmail.com>
2023-07-19 19:33:53 -0500

    demo folder ./components/ ./exceptions/ ./model/ ./repository/ ./service/ DemoApplication.java

---
+++ spring/src/main/java/springboot/ui/spring/demo/exceptions/CommonException.java
@@ -0,0 +1,9 @@
+package springboot.ui.spring.demo.exceptions;
+
+public class CommonException extends RuntimeException {
+    private static final long serialVersionUId = 3080004140659213332L;
+
+    public CommonException(String message) {
+        super(message);
+    }
+}
